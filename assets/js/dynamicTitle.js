98db4f1f230042409ae65c0bb85add970b1f1ca5
yutianfuu <sutang@qunhemail.com>
2022-01-05 11:00:53 -0500

    Update info

--- assets/js/dynamicTitle.js
+++ assets/js/dynamicTitle.js
@@ -16,14 +16,14 @@ window.onload = function () {
   document.addEventListener("visibilitychange", function (e) {
     if (!document.hidden) {
       document.title = pageTitle;
-      favicon.href = "./assets/images/panda.JPG";
+      favicon.href = "./assets/images/photo.jpg";
     }
   });
 
   function toggle() {
     if (document.title === attentionMessage) {
       document.title = pageTitle;
-      favicon.href = "./assets/images/panda.JPG";
+      favicon.href = "./assets/images/photo.jpg";
     } else {
       document.title = attentionMessage;
       favicon.href = "./assets/images/a.png";
