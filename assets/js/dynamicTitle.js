c45517da3cdf91e372440a9893d95de96caecf60
yutianfuu <yutianfuu@gmail.com>
2022-01-15 10:41:17 -0500

    Update

--- assets/js/dynamicTitle.js
+++ assets/js/dynamicTitle.js
@@ -16,14 +16,14 @@ window.onload = function () {
   document.addEventListener("visibilitychange", function (e) {
     if (!document.hidden) {
       document.title = pageTitle;
-      favicon.href = "./assets/images/panda.jpg";
+      favicon.href = "./assets/images/pan.jpg";
     }
   });
 
   function toggle() {
     if (document.title === attentionMessage) {
       document.title = pageTitle;
-      favicon.href = "./assets/images/panda.jpg";
+      favicon.href = "./assets/images/pan.jpg";
     } else {
       document.title = attentionMessage;
       favicon.href = "./assets/images/a.png";
